fe7b5d3d21bd4fabe3781c2df3e504633bc9c71f
lavina_desai <lavina.desai@infoud.co.in>
2017-08-26 12:55:39 +0530

    database rules for user written

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAk0HLgCSXFs4FTyK_WeOYuAWi9_OWUlnI",
+    authDomain: "chatapp-2c59b.firebaseapp.com",
+    databaseURL: "https://chatapp-2c59b.firebaseio.com",
+    projectId: "chatapp-2c59b",
+    storageBucket: "chatapp-2c59b.appspot.com",
+    messagingSenderId: "926459817562"
+  }
 };
