9130a5799642391200e661d51803286a047059cf
jesikawaghela <jesika.waghela@infoud.co.in>
2017-11-14 10:02:53 +0530

    ignore firebase configration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    // firebase
+      apiKey: "AIzaSyD7BuOX7pkAywcUarOEZyOaMiKvARM-QWU",
+      authDomain: "chatapp-140da.firebaseapp.com",
+      databaseURL: "https://chatapp-140da.firebaseio.com",
+      projectId: "chatapp-140da",
+      storageBucket: "chatapp-140da.appspot.com",
+      messagingSenderId: "807091372645"
   }
 };
