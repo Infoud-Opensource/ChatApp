51fb81d876a94c150f9acea61726eb6345038f9a
jesikawaghela <jesika.waghela@infoud.co.in>
2017-11-14 09:46:20 +0530

    chatRoom UI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyD7BuOX7pkAywcUarOEZyOaMiKvARM-QWU",
-    authDomain: "chatapp-140da.firebaseapp.com",
-    databaseURL: "https://chatapp-140da.firebaseio.com",
-    projectId: "chatapp-140da",
-    storageBucket: "chatapp-140da.appspot.com",
-    messagingSenderId: "807091372645"
+    // firebase
   }
 };
